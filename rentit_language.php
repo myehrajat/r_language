c5fa3580d0caf1e0a6951baa619940defba863ed
myehrajat <31361589+myehrajat@users.noreply.github.com>
2018-12-15 13:06:02 +0330

    Initial commit

---
+++ rentit_language.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package RentIt_Language
+ * @version 1.0
+ */
+/*
+Plugin Name: RentIt Language
+Plugin URI: https://wordpress.org/plugins/hello-dolly/
+Description: this plugin change theme language!
+Version: 1.0
+Author URI: https://ma.tt/
+Text Domain: RentIt_Language
+*/
+function load_fa_language(){
+	//load translation
+	$domain = 'rentit';
+	$path = get_stylesheet_directory() . '/languages/'; // adjust to the location of your .mo file
+	
+	$locale = apply_filters( 'theme_locale', 'fa_IR' , $domain );
+	load_textdomain( $domain, $path . $locale . '.mo' );
+	//end load translation
+
+}
+//load_fa_language();
